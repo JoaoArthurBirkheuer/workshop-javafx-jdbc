bb84523aec146103a088b07cd87598c4e3357c00
João Arthur Birkheuer de Cristo <joaobirkheuer@gmail.com>
2025-03-23 13:30:51 -0300

    Validation Exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -65,6 +68,9 @@ public class DepartmentFormController implements Initializable {
 		catch(DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
+		catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
 	}
 	
 	private void notifyDataChangeListeners() {
@@ -74,11 +80,22 @@ public class DepartmentFormController implements Initializable {
 	}
 
 	private Department getFormData() {
+		
+		ValidationException exception = new ValidationException("Validation error");
+		
 		if (department == null) {
 	        throw new IllegalStateException("Department entity was null");
 	    }
-	    department.setId(Utils.tryParseToInt(txtId.getText()));  // Keep using existing instance
-	    department.setName(txtName.getText());
+	    department.setId(Utils.tryParseToInt(txtId.getText()));
+	    
+	    if(txtName.getText() == null || 
+	    		txtName.getText().trim().equals(""))
+	    	exception.addError("name", "Field cannot be empty");
+	    
+	    	department.setName(txtName.getText());
+	    	
+	    if(exception.getErrors().size() > 0) throw exception;
+	    
 	    return department;
 	}
 
@@ -103,6 +120,14 @@ public class DepartmentFormController implements Initializable {
 		
 		txtId.setText(String.valueOf(department.getId()));
 		txtName.setText(department.getName());
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
 	}
 
 }
